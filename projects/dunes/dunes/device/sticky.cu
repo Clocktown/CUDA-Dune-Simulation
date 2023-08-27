0e88d622aaa7ab4f564ebcdda773e861fe22c1b7
Lars Günther <lars.guenther98@googlemail.com>
2023-08-27 14:32:06 +0200

    Removed race conditions in echo dune calculation

--- projects/dunes/dunes/device/sticky.cu
+++ projects/dunes/dunes/device/sticky.cu
@@ -8,28 +8,7 @@
 namespace dunes
 {
 
-//__device__ __forceinline__ void atomicMax(const float* const addr, const float val)
-//{
-//	if (*addr >= val) return;
-//
-//	unsigned int* const uaddr = (unsigned int*)addr;
-//	unsigned int old = *uaddr, assumed;
-//
-//	do
-//	{
-//		assumed = old;
-//
-//		if (__uint_as_float(assumed) >= val)
-//		{
-//			break;
-//		}
-//
-//		old = atomicCAS(uaddr, assumed, __float_as_uint(val));
-//	}
-//	while (assumed != old);
-//}
-
-__global__ void setupStickyKernel(Array2D<float4> t_resistanceArray)
+__global__ void setupStickyKernel(const Array2D<float2> t_terrainArray, const Array2D<float2> t_windArray, Buffer<float> t_cliffBuffer)
 {
 	const int2 index{ getGlobalIndex2D() };
 	const int2 stride{ getGridStride2D() };
@@ -40,15 +19,35 @@ __global__ void setupStickyKernel(Array2D<float4> t_resistanceArray)
 	{
 		for (cell.y = index.y; cell.y < c_parameters.gridSize.y; cell.y += stride.y)
 		{
-			float4 resistance{ t_resistanceArray.read(cell) };
-			resistance.w = 0.0f;
+			const float2 terrain{ t_terrainArray.read(cell) };
+			const float height{ terrain.x + terrain.y };
 
-			t_resistanceArray.write(cell, resistance);
+			const float2 windVelocity = t_windArray.read(cell);
+			const float windSpeed = length(windVelocity);
+			const float2 windDirection = windVelocity / (windSpeed + 1e-06f);
+
+			float2 nextPosition{ make_float2(cell) - windDirection };
+			int2 nextCell{ getWrappedCell(make_int2(nextPosition)) };
+			const float2 nextTerrain{ t_terrainArray.sample(nextPosition) };
+			const float nextHeight{ nextTerrain.x + nextTerrain.y };
+			float cliffHeight{ height - nextHeight };
+			const float angle{ cliffHeight / c_parameters.gridScale };
+
+			const int cellIndex{ getCellIndex(cell) };
+
+			if (angle >= c_parameters.stickyAngle)
+			{
+				t_cliffBuffer[cellIndex] = cliffHeight;
+			}
+			else
+			{
+				t_cliffBuffer[cellIndex] = 0.0f;
+			}
 		}
 	}
 }
 
-__global__ void stickyKernel(const Array2D<float2> t_terrainArray, const Array2D<float2> t_windArray, Array2D<float4> t_resistanceArray)
+__global__ void stickyKernel(const Array2D<float2> t_windArray, Array2D<float4> t_resistanceArray, Buffer<float> t_cliffBuffer)
 {
 	const int2 cell{ getGlobalIndex2D() };
 
@@ -57,59 +56,51 @@ __global__ void stickyKernel(const Array2D<float2> t_terrainArray, const Array2D
 		return;
 	}
 
-	const float2 terrain{ t_terrainArray.read(cell) };
-	const float height{ terrain.x + terrain.y };
-
 	const float2 windVelocity = t_windArray.read(cell);
 	const float windSpeed = length(windVelocity);
 	const float2 windDirection = windVelocity / (windSpeed + 1e-06f);
 
-	float2 nextPosition{ make_float2(cell) - windDirection };
-	int2 nextCell{ getWrappedCell(make_int2(nextPosition)) };
-	const float2 nextTerrain{ t_terrainArray.sample(nextPosition) };
-	const float nextHeight{ nextTerrain.x + nextTerrain.y };
-	float cliffHeight{ height - nextHeight };
-	const float angle{ cliffHeight / c_parameters.gridScale };
+	float2 nextPosition{ make_float2(cell) };
+	const float erosionResistance{ -c_parameters.stickyStrength };
 
-	if (angle >= c_parameters.stickyAngle)
+	for (float distance = c_parameters.gridScale; distance <= c_parameters.maxStickyHeight; distance += c_parameters.gridScale)
 	{
-		const float maxDistance = fminf(cliffHeight, c_parameters.maxStickyHeight);
-		const float erosionResistance{ -c_parameters.stickyStrength };
+		nextPosition += windDirection;
 
-		int cellCount{ static_cast<int>(c_parameters.stickyRange.x * maxDistance / c_parameters.gridScale) };
+		const int2 nextCell{ getWrappedCell(make_int2(nextPosition)) };
+		const int nextCellIndex{ getCellIndex(nextCell) };
+		const float cliffHeight{ t_cliffBuffer[nextCellIndex] };
 
-		for (int i{ 0 }; i < cellCount; ++i)
+		if (cliffHeight > 0.0f)
 		{
 			float4 resistance{ t_resistanceArray.read(nextCell) };
-			resistance.w = erosionResistance;
-			t_resistanceArray.write(nextCell, resistance);
+			const float maxDistance{ fminf(cliffHeight, c_parameters.maxStickyHeight) };
+			const float erosionDistance{ c_parameters.stickyRange.x * maxDistance };
+			const float stickyDistance{ c_parameters.stickyRange.y * maxDistance };
+			
+			if (distance <= erosionDistance)
+			{
+				resistance.w = erosionResistance;
+				t_resistanceArray.write(cell, resistance);
+			}
+			else if (distance <= stickyDistance)
+			{
+				resistance.w = 1.0f - (distance - erosionDistance) / (stickyDistance - erosionDistance);
+				t_resistanceArray.write(cell, resistance);
+			}
 
-			nextPosition -= windDirection;
-			nextCell = getWrappedCell(make_int2(nextPosition));
-		}
-
-		cellCount = static_cast<int>((c_parameters.stickyRange.y - c_parameters.stickyRange.x) * maxDistance / c_parameters.gridScale);
-		const float step{ 1.0f / (c_parameters.stickyStrength * cellCount) };
-		
-		for (int i{ 0 }; i < cellCount; ++i)
-		{
-			float4 resistance{ t_resistanceArray.read(nextCell) };
-			resistance.w = (1.0f - i * step);
-			t_resistanceArray.write(nextCell, resistance);
-
-			nextPosition -= windDirection;
-			nextCell = getWrappedCell(make_int2(nextPosition));
+			break;
 		}
 	}
 }
 
 void sticky(const LaunchParameters& t_launchParameters, const SimulationParameters& t_simulationParameters)
 {
-	setupStickyKernel<<<t_launchParameters.optimalGridSize2D, t_launchParameters.optimalBlockSize2D>>>(t_launchParameters.resistanceArray);
+	setupStickyKernel<<<t_launchParameters.optimalGridSize2D, t_launchParameters.optimalBlockSize2D>>>(t_launchParameters.terrainArray, t_launchParameters.windArray, t_launchParameters.tmpBuffer);
 	
 	if (t_simulationParameters.stickyStrength > 0.0f)
 	{
-		stickyKernel<<<t_launchParameters.gridSize2D, t_launchParameters.blockSize2D>>>(t_launchParameters.terrainArray, t_launchParameters.windArray, t_launchParameters.resistanceArray);
+		stickyKernel<<<t_launchParameters.gridSize2D, t_launchParameters.blockSize2D>>>(t_launchParameters.windArray, t_launchParameters.resistanceArray, t_launchParameters.tmpBuffer);
 	}
 }
 
