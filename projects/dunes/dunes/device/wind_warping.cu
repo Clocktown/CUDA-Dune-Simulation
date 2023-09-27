ce3a61b55ee821bf7837575053a440f3ae31683f
Alexander Maximilian Nilles <nillesmax@googlemail.com>
2023-09-27 19:22:38 +0200

    Add alternative wind warping implementation

--- projects/dunes/dunes/device/wind_warping.cu
+++ projects/dunes/dunes/device/wind_warping.cu
@@ -116,7 +116,7 @@ __global__ void readBackSmoothTerrainKernel(Array2D<float2> t_terrainArray, Wind
 		{
 			const int cellIndex{ getCellIndex(cell) };
 			
-			t_terrainArray.write(cell, float2{ t_windWarping.smoothedHeights[1][cellIndex].x, 0.f });
+			t_terrainArray.write(cell, float2{ t_windWarping.smoothedHeights[0][cellIndex].x, 0.f });
 		}
 	}
 }
@@ -169,7 +169,7 @@ __global__ void windWarpingKernel(Array2D<float2> t_windArray, WindWarping t_win
 			const int cellIndex{ getCellIndex(cell) };
 			const float2 windVelocity{ t_windArray.read(cell) };
 			const float windSpeed{ length(windVelocity) };
-			const float2 windDirection{ windVelocity / (windSpeed + 0.000001f) };
+			//const float2 windDirection{ windVelocity / (windSpeed + 0.000001f) };
 			
 			float2 warpDirection{ 0.0f, 0.0f };
 			float weight{ 0.0f };
@@ -188,7 +188,7 @@ __global__ void windWarpingKernel(Array2D<float2> t_windArray, WindWarping t_win
 				const float gradientLength{ length(gradient) };
 				
 				float2 orthogonalDirection{ -gradient.y, gradient.x };
-				orthogonalDirection *= sign(dot(windDirection, orthogonalDirection));
+				orthogonalDirection *= sign(dot(windVelocity, orthogonalDirection));
 				
 				float alpha{ fminf(gradientLength, 1.0f) }; 
 			
@@ -203,6 +203,57 @@ __global__ void windWarpingKernel(Array2D<float2> t_windArray, WindWarping t_win
 
 			warpDirection /= (length(warpDirection) + 0.000001f);
 			t_windArray.write(cell, warpDirection * windSpeed);
+		}
+	}
+}
+
+__global__ void windWarpingKernelImproved(Array2D<float2> t_windArray, WindWarping t_windWarping)
+{
+	const int2 index{ getGlobalIndex2D() };
+	const int2 stride{ getGridStride2D() };
+
+	int2 cell;
+
+	for (cell.x = index.x; cell.x < c_parameters.gridSize.x; cell.x += stride.x)
+	{
+		for (cell.y = index.y; cell.y < c_parameters.gridSize.y; cell.y += stride.y)
+		{
+			const int cellIndex{ getCellIndex(cell) };
+			const float2 windVelocity{ t_windArray.read(cell) };
+			const float windSpeed{ length(windVelocity) };
+			//const float2 windDirection{ windVelocity / (windSpeed + 0.000001f) };
+			
+			float warpAngle{ 0.0f };
+			float weight{ 0.0f };
+
+			for (int i{ 0 }; i < t_windWarping.count; ++i)
+			{
+				const float smoothedHeights[4]{ t_windWarping.smoothedHeights[i][getCellIndex(getWrappedCell(cell + int2{ -1, 0 }))].x,
+								                t_windWarping.smoothedHeights[i][getCellIndex(getWrappedCell(cell + int2{ 1, 0 }))].x,
+								                t_windWarping.smoothedHeights[i][getCellIndex(getWrappedCell(cell + int2{ 0, -1 }))].x,
+								                t_windWarping.smoothedHeights[i][getCellIndex(getWrappedCell(cell + int2{ 0, 1 }))].x };
+
+				const float scale{ t_windWarping.gradientStrengths[i] * 0.5f * c_parameters.rGridScale };
+				const float2 gradient{ scale * (smoothedHeights[1] - smoothedHeights[0]),
+								       scale * (smoothedHeights[3] - smoothedHeights[2]) };
+
+				const float gradientLength{ length(gradient) };
+				
+				float2 orthogonalDirection{ -gradient.y, gradient.x };
+				orthogonalDirection *= sign(dot(windVelocity, orthogonalDirection));
+				
+				float alpha{ fminf(gradientLength / windSpeed, 1.0f) }; 
+			
+				warpAngle += t_windWarping.strengths[i] * alpha * signed_angle(windVelocity, orthogonalDirection);
+				weight += t_windWarping.strengths[i];
+			}
+
+			if (weight > 0.0f)
+			{
+				warpAngle /= weight;
+			}
+
+			t_windArray.write(cell, rotate(windVelocity, warpAngle));
 		}
 	}
 }
@@ -239,6 +290,8 @@ void windWarping(const LaunchParameters& t_launchParameters)
 	    {
 		    CUFFT_CHECK_ERROR(cufftExecC2C(t_launchParameters.fftPlan, t_launchParameters.windWarping.smoothedHeights[i], t_launchParameters.windWarping.smoothedHeights[i], CUFFT_INVERSE));
 	    }
+
+		//readBackSmoothTerrainKernel << < t_launchParameters.optimalGridSize2D, t_launchParameters.optimalBlockSize2D >> > (t_launchParameters.terrainArray, t_launchParameters.windWarping);
 
 	    windWarpingKernel<<<t_launchParameters.optimalGridSize2D, t_launchParameters.optimalBlockSize2D>>>(t_launchParameters.windArray, t_launchParameters.windWarping);
 	}
